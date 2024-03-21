6dbcfd57fe9a18e61c7261f345f18fad146c7b1b
R1ech <164198891+R1ech@users.noreply.github.com>
2024-03-21 05:41:19 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['0548603911']
+global.ownername = "R1ech"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
